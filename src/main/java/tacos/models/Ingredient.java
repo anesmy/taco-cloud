350e26dc629ade11b9e5424b25d8d7dbf40ac563
Nesmy <anastasiia.mudra.kn.2021@lpnu.ua>
2023-01-11 16:19:12 +0200

    Working with Spring Data JDBC

--- src/main/java/tacos/models/Ingredient.java
+++ src/main/java/tacos/models/Ingredient.java
@@ -1,12 +1,29 @@
 package tacos.models;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.relational.core.mapping.Table;
 
 @Data
-public class Ingredient {
+@Table
+@AllArgsConstructor
+@NoArgsConstructor(access= AccessLevel.PRIVATE, force=true)
+public class Ingredient implements Persistable<String> {
+
+    @Id
     private final String id;
     private final String name;
     private final Type type;
+
+    @Override
+    public boolean isNew() {
+        return true;            //false
+    }
+
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
     }
