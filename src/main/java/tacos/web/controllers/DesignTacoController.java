eb85fef15b37b6bf4b26cc355ff400f4bb2655cf
Nesmy <anastasiia.mudra.kn.2021@lpnu.ua>
2023-01-19 13:49:13 +0200

    Preloading data with CommandLineRunner

--- src/main/java/tacos/web/controllers/DesignTacoController.java
+++ src/main/java/tacos/web/controllers/DesignTacoController.java
@@ -1,11 +1,12 @@
 package tacos.web.controllers;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -53,7 +54,6 @@ public class DesignTacoController {
         if (errors.hasErrors()){
             return "design";
         }
-
         // Save the taco...
         // We'll do this in chapter 3
         log.info("Processing taco: " + taco);
@@ -67,5 +67,21 @@ public class DesignTacoController {
                 .stream()
                 .filter(x -> x.getType().equals(type))
                 .collect(Collectors.toList());
+    }
+
+    @Bean
+    public CommandLineRunner dataLoader(IngredientRepository repo) {
+        return args -> {
+            repo.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
+            repo.save(new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
+            repo.save(new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
+            repo.save(new Ingredient("CARN", "Carnitas", Type.PROTEIN));
+            repo.save(new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
+            repo.save(new Ingredient("LETC", "Lettuce", Type.VEGGIES));
+            repo.save(new Ingredient("CHED", "Cheddar", Type.CHEESE));
+            repo.save(new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
+            repo.save(new Ingredient("SLSA", "Salsa", Type.SAUCE));
+            repo.save(new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+        };
     }
 }
