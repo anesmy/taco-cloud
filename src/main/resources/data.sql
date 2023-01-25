2bd1512ee5c2a3a262d87d48ffad47c3fc278df9
Nesmy <anastasiia.mudra.kn.2021@lpnu.ua>
2023-01-25 16:33:56 +0200

    add schema name tacocloud

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,25 +1,25 @@
-delete from Ingredient_Ref;
-delete from Taco;
-delete from Taco_Order;
-delete from Ingredient;
+delete from tacocloud.Ingredient_Ref;
+delete from tacocloud.Taco;
+delete from tacocloud.Taco_Order;
+delete from tacocloud.Ingredient;
 
-insert into Ingredient (id, name, type)
+insert into tacocloud.Ingredient (id, name, type)
 values ('FLTO', 'Flour Tortilla', 'WRAP');
-insert into Ingredient (id, name, type)
+insert into tacocloud.Ingredient (id, name, type)
 values ('COTO', 'Corn Tortilla', 'WRAP');
-insert into Ingredient (id, name, type)
+insert into tacocloud.Ingredient (id, name, type)
 values ('GRBF', 'Ground Beef', 'PROTEIN');
-insert into Ingredient (id, name, type)
+insert into tacocloud.Ingredient (id, name, type)
 values ('CARN', 'Carnitas', 'PROTEIN');
-insert into Ingredient (id, name, type)
+insert into tacocloud.Ingredient (id, name, type)
 values ('TMTO', 'Diced Tomatoes', 'VEGGIES');
-insert into Ingredient (id, name, type)
+insert into tacocloud.Ingredient (id, name, type)
 values ('LETC', 'Lettuce', 'VEGGIES');
-insert into Ingredient (id, name, type)
+insert into tacocloud.Ingredient (id, name, type)
 values ('CHED', 'Cheddar', 'CHEESE');
-insert into Ingredient (id, name, type)
+insert into tacocloud.Ingredient (id, name, type)
 values ('JACK', 'Monterrey Jack', 'CHEESE');
-insert into Ingredient (id, name, type)
+insert into tacocloud.Ingredient (id, name, type)
 values ('SLSA', 'Salsa', 'SAUCE');
-insert into Ingredient (id, name, type)
+insert into tacocloud.Ingredient (id, name, type)
 values ('SRCR', 'Sour Cream', 'SAUCE');
