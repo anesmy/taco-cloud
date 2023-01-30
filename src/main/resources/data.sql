35de9bad4b54c35c2f700a918d9f12877434805c
Nesmy <anastasiia.mudra.kn.2021@lpnu.ua>
2023-01-30 16:46:37 +0200

    Resolving of conflict after Merge

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,46 +3,24 @@ delete from tacocloud.Taco;
 delete from tacocloud.Taco_Order;
 delete from tacocloud.Ingredient;
 
-<<<<<<< HEAD
---insert into Ingredient (id, name, type)
+--insert into tacocloud.Ingredient (id, name, type)
 --values ('FLTO', 'Flour Tortilla', 'WRAP');
---insert into Ingredient (id, name, type)
+--insert into tacocloud.Ingredient (id, name, type)
 --values ('COTO', 'Corn Tortilla', 'WRAP');
---insert into Ingredient (id, name, type)
+--insert into tacocloud.Ingredient (id, name, type)
 --values ('GRBF', 'Ground Beef', 'PROTEIN');
---insert into Ingredient (id, name, type)
+--insert into tacocloud.Ingredient (id, name, type)
 --values ('CARN', 'Carnitas', 'PROTEIN');
---insert into Ingredient (id, name, type)
+--insert into tacocloud.Ingredient (id, name, type)
 --values ('TMTO', 'Diced Tomatoes', 'VEGGIES');
---insert into Ingredient (id, name, type)
+--insert into tacocloud.Ingredient (id, name, type)
 --values ('LETC', 'Lettuce', 'VEGGIES');
---insert into Ingredient (id, name, type)
+--insert into tacocloud.Ingredient (id, name, type)
 --values ('CHED', 'Cheddar', 'CHEESE');
---insert into Ingredient (id, name, type)
+--insert into tacocloud.Ingredient (id, name, type)
 --values ('JACK', 'Monterrey Jack', 'CHEESE');
---insert into Ingredient (id, name, type)
+--insert into tacocloud.Ingredient (id, name, type)
 --values ('SLSA', 'Salsa', 'SAUCE');
---insert into Ingredient (id, name, type)
+--insert into tacocloud.Ingredient (id, name, type)
 --values ('SRCR', 'Sour Cream', 'SAUCE');
-=======
-insert into tacocloud.Ingredient (id, name, type)
-values ('FLTO', 'Flour Tortilla', 'WRAP');
-insert into tacocloud.Ingredient (id, name, type)
-values ('COTO', 'Corn Tortilla', 'WRAP');
-insert into tacocloud.Ingredient (id, name, type)
-values ('GRBF', 'Ground Beef', 'PROTEIN');
-insert into tacocloud.Ingredient (id, name, type)
-values ('CARN', 'Carnitas', 'PROTEIN');
-insert into tacocloud.Ingredient (id, name, type)
-values ('TMTO', 'Diced Tomatoes', 'VEGGIES');
-insert into tacocloud.Ingredient (id, name, type)
-values ('LETC', 'Lettuce', 'VEGGIES');
-insert into tacocloud.Ingredient (id, name, type)
-values ('CHED', 'Cheddar', 'CHEESE');
-insert into tacocloud.Ingredient (id, name, type)
-values ('JACK', 'Monterrey Jack', 'CHEESE');
-insert into tacocloud.Ingredient (id, name, type)
-values ('SLSA', 'Salsa', 'SAUCE');
-insert into tacocloud.Ingredient (id, name, type)
-values ('SRCR', 'Sour Cream', 'SAUCE');
->>>>>>> JPA
+
