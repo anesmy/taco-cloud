934d35e484ca4a954e6e2a2598da5ab4e94b0e63
Nesmy <anastasiia.mudra.kn.2021@lpnu.ua>
2023-02-16 17:16:20 +0200

    Sending data to the server (@PostMapping) and Updating data on the server (@PutMapping and @PatchMapping)

--- src/main/java/tacos/web/api/TacoController.java
+++ src/main/java/tacos/web/api/TacoController.java
@@ -6,8 +6,10 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import tacos.data.OrderRepository;
 import tacos.data.TacoRepository;
 import tacos.models.Taco;
+import tacos.models.TacoOrder;
 
 import java.util.Optional;
 
@@ -17,9 +19,11 @@ import java.util.Optional;
 public class TacoController {
 
     private TacoRepository tacoRepo;
+    private OrderRepository orderRepo;
 
-    public TacoController(TacoRepository tacoRepo) {
+    public TacoController(TacoRepository tacoRepo, OrderRepository orderRepo) {
         this.tacoRepo = tacoRepo;
+        this.orderRepo = orderRepo;
     }
 
     @GetMapping(params="recent")
@@ -44,4 +48,56 @@ public class TacoController {
         //return optTaco.map(taco -> new ResponseEntity<>(taco, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
     }
 
+    @PostMapping(consumes="application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Taco postTaco(@RequestBody Taco taco) {
+        return tacoRepo.save(taco);
+    }
+
+    @PutMapping(path="/{orderId}", consumes="application/json")
+    public TacoOrder putOrder(@PathVariable("orderId") Long orderId,
+                              @RequestBody TacoOrder order) {
+
+        order.setId(orderId);
+        return orderRepo.save(order);
+    }
+
+    @PatchMapping(path="/{orderId}", consumes="application/json")
+    public ResponseEntity<TacoOrder> patchOrder(@PathVariable("orderId") Long orderId,
+                                @RequestBody TacoOrder patch) {
+
+        Optional<TacoOrder> optTacoOrder = orderRepo.findById(orderId);
+        if (optTacoOrder.isEmpty()) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+
+        TacoOrder order = optTacoOrder.get();
+
+        if (patch.getDeliveryName() != null) {
+            order.setDeliveryName(patch.getDeliveryName());
+        }
+        if (patch.getDeliveryStreet() != null) {
+            order.setDeliveryStreet(patch.getDeliveryStreet());
+        }
+        if (patch.getDeliveryCity() != null) {
+            order.setDeliveryCity(patch.getDeliveryCity());
+        }
+        if (patch.getDeliveryState() != null) {
+            order.setDeliveryState(patch.getDeliveryState());
+        }
+        if (patch.getDeliveryZip() != null) {
+            order.setDeliveryZip(patch.getDeliveryState());
+        }
+        if (patch.getCcNumber() != null) {
+            order.setCcNumber(patch.getCcNumber());
+        }
+        if (patch.getCcExpiration() != null) {
+            order.setCcExpiration(patch.getCcExpiration());
+        }
+        if (patch.getCcCVV() != null) {
+            order.setCcCVV(patch.getCcCVV());
+        }
+
+        return new ResponseEntity<>(order, HttpStatus.OK);
+    }
 }
